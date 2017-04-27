82f5406fce190979728d8c3f8ff48e297d20ca48
robertjosephwayne <robertjwayne@gmail.com>
2017-04-26 23:21:27 -0400

    Clean up instruction comments

--- app.rb
+++ app.rb
@@ -1,17 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # The name route should display "My name is __" in the browser
   get '/name' do
     "My name is _____"
   end
 
-  # The hometown route should display "My hometown is __"
   get '/hometown' do
     "My hometown is _____"
   end
 
-  # The favorite-song route should display "My favorite song is __"
   get '/favorite-song' do
     "My favorite song is _____"
   end
